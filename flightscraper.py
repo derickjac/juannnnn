4c19adb724f6220364fa25fb7c3c59b4f6d3cfdd
Griffin <35238739+PolarisGriffin@users.noreply.github.com>
2018-11-03 19:47:20 -0500

    goof

--- flightscraper.py
+++ flightscraper.py
@@ -1,1 +1,12 @@
-import selenium
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.set_window_size(1024, 768)
+
+driver.get("https://google.com")
+
+element = driver.find_element_by_name('q')
+element.send_keys('testing') 
+#btnK
+element.submit()
